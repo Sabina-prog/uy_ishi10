2fe6d51e9eb0f5812206b160d882c237e6ccc103
Sabina <s.y.abduraufova@gmail.com>
2026-07-23 11:49:42 +0500

    vazifa

--- preparation.py
+++ preparation.py
@@ -24,7 +24,100 @@
 # print(new)
 # lst=new
 # print(lst)
+# ---------------------------------
+# MOCK
+# 1 chi misol
+# dct={
+#     "yanvar":"01",
+#      "fevral": "02",
+#      "mart" : "03",
+#      "aprel":"04",
+#      "may": "05",
+#      "iyun" : "06",
+#      "iyul":"07",
+#      "avgust": "08",
+#      "sentyabr" : "09",
+#     "oktyabr":"10",
+#      "noyabr": "11",
+#      "decabr" : "12"
 
+# }
+# n="24 mart 2025"
+# print(n[2:-4])
+# s=n.split()
+# print(f"{s[0]}.{dct[s[1]]}.{s[2]}")
+# print(f"{n[:2]}.{n[2:-4]}.{n[-2:]}")
+# ---------------------------------------
+# 2 chi misol
+# numbers=[3,7,12,7,5,3,9,12,15,7]
+# n=tuple(set(numbers))
+# orta=sum(n)/len(n)
+# print(orta)
+# -----------------------------------
+# 3 chi misol
+# import json
+
+# product={
+#     "olma": 12000,
+#     "banan":18000,
+#     "shaftoli": 15000,
+#     "uzum":20000
+# }
+
+# maxsulot=input("maxsulot nomini kriting: ").lower()
+# miqdor=int(input("nechta olishingizni kriting: "))
+
+# if maxsulot in product:
+#     narx=product[maxsulot]*miqdor
+
+#     data={
+#         "maxsulot":maxsulot,
+#         "miqdor":miqdor,
+#         "narx": narx
+#     }
+#     print(f"umumiy narx={narx}")
+
+#     with open("dokon.json","w") as file:
+#         json.dump(data,file,indent=4)
+
+
+# else:
+#     print("bunday maxsulot mavjut emas.")
+# ---------------------------------------------------------------
+# 4 chi misol
+# lst=input("matn kiriting: ")
+# lst=lst.split()
+# print(lst)
+# uzun=max(lst,key=len)
+
+# print(uzun)
+# print(t:=sorted(lst))
+# with open("function.txt","w") as f:
+#     print(" ".join(t))
+
+import json 
+
+with open("test.json") as f:
+    data = json.load(f)
+    dct = {"payment":0}
+    # for branch in data["branches"]: 
+    #     for student in branch['students']:
+    #         if dct["payment"] < student['payment']:
+    #             dct = student.copy()
+    #             dct["branch"] = branch['name']
+                # print(branch)
+    print(dct)
+
+    lst = []
+    for branch in data['branches']:
+        lst += branch['students']
+    print(max(lst, key=lambda x: x['payment']))
+
+
+
+            
+        
+            
 
 
 
--- uyga_vazifa10.py
+++ uyga_vazifa10.py
@@ -2,6 +2,49 @@
 import json
 with open("uyga_vazifa10.json") as f:
     dct=json.load(f)
-    new=dct["branches"]
-    for i  in new:
-        print(i["name"])
+    # new=dct["branches"]
+    # for i  in new:
+    #     print(i["name"])
+# --------------------------------------------
+# 2 chi misol
+    # for i in dct["branches"]:
+    #     for x in i["teachers"]:
+    #         if x["subject"]=="Python":
+    #             print(x["name"],i["name"],x["experience"],x["subject"])
+# ---------------------------------------------------------------
+# 3 chi misol
+    # for i in dct["branches"]:
+    #     count=0
+    #     for x in i["students"]:
+    #         count+=1
+    #     print(i["name"],count)
+# -----------------------------------------------------------
+# 4 chi misol
+# for i in dct["branches"]:
+#     a={"payment":0}
+#     for x in i["students"]:
+#         if x["payment"]>a["payment"]:
+#             a=x.copy()
+#             a["branch"]=i["name"]
+# print(a["name"],a["branch"])
+# -------------------------------------------
+# 5 chi misol
+    # for i in dct["branches"]:
+    #     sum=0
+    #     for x in i["students"]:
+    #         sum+=x["payment"]
+    #     print(i["name"],sum)
+# -------------------------------------
+# 6 chi misol
+    # for i in dct["branches"]:
+    #     for x in i["teachers"]:
+    #         if x["experience"]>5:
+    #             print(x["name"],x["experience"])
+# -----------------------------------------------------
+# 7 chi misol
+    # for i in dct["branches"]:
+    #     for x in i["teachers"]:
+    #         if x["subject"]!="Python":
+    #             break
+    #     else:
+    #         print(i["name"])
